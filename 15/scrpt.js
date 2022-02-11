1b5661efed911bd3f6f378ee62cf2706b24ffc35
Luiz18g <94083947+Luiz18g@users.noreply.github.com>
2022-02-10 22:19:24 -0300

    exercicio feito

---
+++ 15/scrpt.js
@@ -0,0 +1,9 @@
+let idade
+console.log("informe sua idade")
+idade = 15
+console.log("a idade informada foi:",idade)
+if(idade >= 18){
+    console.log("voce e maior de idade")
+} else {
+    console.log("voce e menor de idade")
+}
