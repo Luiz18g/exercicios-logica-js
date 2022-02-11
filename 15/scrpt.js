301e80d142ccc663f100e2edd2f163e600e555c3
Luiz18g <94083947+Luiz18g@users.noreply.github.com>
2022-02-10 22:21:25 -0300

    atualização

--- 15/scrpt.js
+++ 15/scrpt.js
@@ -5,5 +5,5 @@ console.log("a idade informada foi:",idade)
 if(idade >= 18){
     console.log("voce e maior de idade")
 } else {
-    console.log("voce e menor de idade")
+    console.log("voce não pode dirigir")
 }
